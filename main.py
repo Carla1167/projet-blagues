dd63d637cb2167362af16057ef4b14461a30ed18
Carla <carla1ere.simon@gmail.com>
2025-03-12 10:04:28 +0100

    Modification de main.py par Développeur A

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import pyjokes
 def tell_joke(lang="en"):
-    print(pyjokes.get_joke(language=lang))
+    print("Blague modifiée par Développeur A")
 
-if __name__=="__main__":
-    lang = input("Choisissez une langue (en, de, es, it, gl):")
-    tell_joke(lang)
